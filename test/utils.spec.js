b6b1506c6dc3116e61416c359a24056b88aa4fc5
nartek01 <nartek01@gmail.com>
2020-08-29 16:26:40 +0200

    Removed the old files, due to inaccurate use of assert functions.

--- test/utils.spec.js
+++
@@ -1,8 +0,0 @@
-const assert = require('assert'),
-{ checkAge } = require('../utils/utils.js')
-
-describe('blackjack.js', function () {  //"describe" grupperar en eller flera tester 
-  it('Check if user is abve/or 18 years old,otherwise throw error', function () { //Varje test skapas med ett "it" anrop, som beskriver testen
-    assert(checkAge() >= 18, 19) //och en testfunktion.
-  })
-})
